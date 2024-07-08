ab863209f85d2d60922bfe530596943d2adbb6ee
Kai Zhang <cskaizhang@gmail.com>
2024-07-08 21:41:54 +0800

    Update model_base.py

--- models/model_base.py
+++ models/model_base.py
@@ -7,7 +7,11 @@ class ModelBase():
     def __init__(self, opt):
         self.opt = opt                         # opt
         self.save_dir = opt['path']['models']  # save models
-        self.device = torch.device('cuda' if opt['gpu_ids'] is not None else 'cpu')
+        # self.device = torch.device('cuda' if opt['gpu_ids'] is not None else 'cpu')
+        if opt['gpu_ids'] is not None:
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+        else:
+            self.device = torch.device("cpu")
         self.is_train = opt['is_train']        # training or not
         self.schedulers = []                   # schedulers
         self.amp_scaler = torch.cuda.amp.GradScaler(enabled=self.opt['use_amp'])
