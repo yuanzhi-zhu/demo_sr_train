212a67e70062d71c6cff8047a6969d78edf57c5f
yuazhu-zhu <zyzeroer@gmail.com>
2024-10-21 00:09:20 +0800

    bash script

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-## Simple training demo for SISR
-
-
-
-
+## Simple training demo for SISR with MSRResNet
 
 ```python
 python main_train_psnr.py --opt options/train_msrresnet_psnr.json
+```
+
+or use the run.sh to specify the device(s) through
+
+```bash
+bash run.sh
 ```
--- run.sh
+++ run.sh
@@ -7,14 +7,28 @@ echo " "
 
 ###########################################################################################################
 
-# run with single GPU
+# run PSNR with single GPU
 CUDA_VISIBLE_DEVICES=0 torchrun --standalone --nproc_per_node=1 \
             main_train_psnr.py --opt options/train_msrresnet_psnr.json
 
-# # run with two GPUs
+# # run PSNR with two GPUs
 # CUDA_VISIBLE_DEVICES=0,1 torchrun --standalone --nproc_per_node=2 \
 #             main_train_psnr.py --opt options/train_msrresnet_psnr.json
 
-# # run with cpu
-# CUDA_VISIBLE_DEVICES=0 torchrun --standalone --nproc_per_node=1 \
+# # run PSNR with cpu
+# CUDA_VISIBLE_DEVICES='' torchrun --standalone --nproc_per_node=1 \
 #             main_train_psnr.py --opt options/train_msrresnet_psnr.json
+
+###########################################################################################################
+
+# # run GAN with single GPU
+# CUDA_VISIBLE_DEVICES=0 torchrun --standalone --nproc_per_node=1 \
+#             main_train_gan.py --opt options/train_msrresnet_gan.json
+
+# # run PSNR with two GPUs
+# CUDA_VISIBLE_DEVICES=0,1 torchrun --standalone --nproc_per_node=2 \
+#             main_train_gan.py --opt options/train_msrresnet_gan.json
+
+# # run PSNR with cpu
+# CUDA_VISIBLE_DEVICES='' torchrun --standalone --nproc_per_node=1 \
+#             main_train_gan.py --opt options/train_msrresnet_gan.json
